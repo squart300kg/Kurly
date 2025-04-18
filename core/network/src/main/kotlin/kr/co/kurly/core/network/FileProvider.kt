9f013f14391ada82ab09db094f8f1ef8f7e3cf26
sysong <sysong@pentasecurity.com>
2025-04-18 13:34:09 +0900

    doc: FileProvider내, 가이드 주석 추가

--- core/network/src/main/kotlin/kr/co/kurly/core/network/FileProvider.kt
+++ core/network/src/main/kotlin/kr/co/kurly/core/network/FileProvider.kt
@@ -2,6 +2,8 @@ package kr.co.kurly.core.network
 
 import android.content.Context
 
+// 원래 해당 코드는 'network'프로젝트가 아닌, 'file' 또는 'local'과 같은 프로젝트에 존재해야 합니다.
+// 하지만 RestAPI연동했을 때를 가정하여 코드를 작성했기에 'network'프로젝트에 위치시킵니다.
 interface FileProvider {
   fun getJsonFromAsset(fileName: String): String
 }
