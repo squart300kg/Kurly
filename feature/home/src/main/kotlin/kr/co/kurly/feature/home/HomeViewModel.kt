01720554dadd129369d45a1fcf15875ba6e738f8
sysong <sysong@pentasecurity.com>
2025-04-21 12:29:46 +0900

    feat: 새로고침, 페이징에 따른 찜하기 데이터 동기화 완료

--- feature/home/src/main/kotlin/kr/co/kurly/feature/home/HomeViewModel.kt
+++ feature/home/src/main/kotlin/kr/co/kurly/feature/home/HomeViewModel.kt
@@ -4,31 +4,20 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import kr.co.kurly.core.domain.GetProductsUseCase
-import kr.co.kurly.core.domain.model.SectionProductDomainResponse
-import kr.co.kurly.core.model.SectionType
 import kr.co.kurly.core.repository.ProductRepository
 import kr.co.kurly.core.repository.dto.FavoriteMakingDtoRequest
-import kr.co.kurly.core.repository.dto.SectionDtoResponse
+import kr.co.kurly.core.repository.dto.FavoriteMakingDtoResponse
 import kr.co.kurly.core.ui.BaseViewModel
-import kr.co.kurly.core.ui.PriceLineType
-import kr.co.kurly.core.ui.PriceUiModel
-import kr.co.kurly.core.ui.ProductSectionType
-import kr.co.kurly.core.ui.ProductUiModel
-import kr.co.kurly.core.ui.UiEvent
-import kr.co.kurly.core.ui.UiSideEffect
-import kr.co.kurly.core.ui.UiState
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
@@ -75,31 +64,26 @@ class HomeViewModel @Inject constructor(
     }
   }
 
+  private val cachedProductFavoriteIds = MutableSharedFlow<List<FavoriteMakingDtoResponse>>(
+    replay = 1,
+    onBufferOverflow = BufferOverflow.DROP_OLDEST
+  )
+
   init {
     setEffect { HomeUiSideEffect.Load.First }
 
     viewModelScope.launch {
       productRepository.observeAllFavoriteIds()
-        .onEach {
-          while(uiState.value.homeUiModels.isEmpty()) {
-            delay(100L)
-          }
-        }
         .collect { dtoResponses ->
-          val updatedHomeUiModels = uiState.value.homeUiModels.map { homeUiModel ->
-            val updatedProductUiModels = homeUiModel.productUiModels.map { productUiModel ->
-              val isFavorite = dtoResponses.any {
-                  it.sectionId == homeUiModel.section.id &&
-                  it.productId == productUiModel.id
-              }
-              productUiModel.copy(isFavorite = isFavorite)
-            }.toPersistentList()
-
-            homeUiModel.copy(productUiModels = updatedProductUiModels)
-          }.toPersistentList()
-
+          cachedProductFavoriteIds.emit(dtoResponses)
           setState {
-            copy(homeUiModels = updatedHomeUiModels)
+            val homeUiModels = homeUiModels.syncWithFavoriteIdsAndGet(dtoResponses)
+            copy(
+              uiType =
+              if (homeUiModels.isNotEmpty()) HomeUiType.LOADED
+              else HomeUiType.NONE,
+              homeUiModels = homeUiModels
+            )
           }
         }
     }
@@ -133,7 +117,8 @@ class HomeViewModel @Inject constructor(
           }
         }
         .catch { setErrorState(it) }
-        .collect {
+        .collect { domainResponse ->
+          val favoriteIds = cachedProductFavoriteIds.first()
           setState {
             copy(
               uiType = HomeUiType.LOADED,
@@ -141,17 +126,35 @@ class HomeViewModel @Inject constructor(
               when (loadType) {
                 is HomeUiSideEffect.Load.First,
                 is HomeUiSideEffect.Load.Refresh -> {
-                  HomeUiModel.mapperToUiModel(it)
+                  HomeUiModel.mapperToUiModel(domainResponse).syncWithFavoriteIdsAndGet(favoriteIds)
                 }
                 is HomeUiSideEffect.Load.More -> {
                   (homeUiModels as PersistentList)
-                    .addAll(HomeUiModel.mapperToUiModel(it))
+                    .addAll(HomeUiModel.mapperToUiModel(domainResponse)).syncWithFavoriteIdsAndGet(favoriteIds)
                 }
               },
-              nextPage = it.nextPage
+              nextPage = domainResponse.nextPage
             )
           }
         }
     }
+  }
+
+  private fun ImmutableList<HomeUiModel>.syncWithFavoriteIdsAndGet(
+    dtoResponses: List<FavoriteMakingDtoResponse>
+  ): ImmutableList<HomeUiModel> {
+    val updatedHomeUiModels = this.map { homeUiModel ->
+      val updatedProductUiModels = homeUiModel.productUiModels.map { productUiModel ->
+        val isFavorite = dtoResponses.any {
+          it.sectionId == homeUiModel.section.id &&
+            it.productId == productUiModel.id
+        }
+        productUiModel.copy(isFavorite = isFavorite)
+      }.toPersistentList()
+
+      homeUiModel.copy(productUiModels = updatedProductUiModels)
+    }.toPersistentList()
+
+    return updatedHomeUiModels
   }
 }
