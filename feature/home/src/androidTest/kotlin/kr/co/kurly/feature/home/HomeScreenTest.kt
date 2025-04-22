a6ba4d7d6d88e011ec9f071a82f7ebbcd2bdefd2
sysong <sysong@pentasecurity.com>
2025-04-22 12:20:16 +0900

    test: whenFirstUiLoaded_thenShowLoadedTypeUi 작성

--- feature/home/src/androidTest/kotlin/kr/co/kurly/feature/home/HomeScreenTest.kt
+++ feature/home/src/androidTest/kotlin/kr/co/kurly/feature/home/HomeScreenTest.kt
@@ -2,7 +2,10 @@ package kr.co.kurly.feature.home
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import kr.co.kurly.feature.home.model.HomeUiStateTestData
+import kr.co.kurly.test.testing.ui.TestTag.HORIZONTAL_ITEMS
+import kr.co.kurly.test.testing.ui.TestTag.PRODUCT_LIST
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,12 +15,15 @@ class HomeScreenTest {
   val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
   @Test
-  fun whenFirstUiLoaded_thenShowLoadedTypeUi() {
+  fun whenFirstUiLoading_thenShowNoneTypeUi() {
     composeTestRule.setContent {
       HomeScreen(
         uiState = HomeUiStateTestData.initState
       )
     }
+    composeTestRule
+      .onNodeWithTag(PRODUCT_LIST)
+      .assertDoesNotExist()
   }
 
 }
